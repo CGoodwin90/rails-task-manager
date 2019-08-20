c935f3d42abcf98ee9b18f730037e02b6a756dfe
Chris Goodwin <chris_2190@hotmail.com>
2019-08-20 15:53:53 +1000

    Refactored Program

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new', as: :task_new
-  post 'tasks', to: 'tasks#create', as: :task_create
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
-  patch 'tasks/:id', to: 'tasks#update', as: :task_update
-  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
+  # get 'tasks', to: 'tasks#index'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
